1ae523e8e9b0f04a6232688351f3d71e8ce5c441
Daniel Evers <dermojo@gmail.com>
2019-02-23 20:52:37 +0100

    superfluous files [skip ci]

--- run_cmake.bat
+++
@@ -1,3 +0,0 @@
-set GTEST=g:\spsl\googletest-release-1.8.0
-set buildcfg=Release
-cmake -DGTEST_LIBRARY=%GTEST%\build\%buildcfg%\gtest.lib -DGTEST_MAIN_LIBRARY=%GTEST%\build\%buildcfg%\gtest_main.lib -DGTEST_INCLUDE_DIR=%GTEST%\googletest\include ..
