7480fdf3db813d99641912d734bc88028ab1f6e3
Daniel Evers <dermojo@gmail.com>
2019-02-27 20:37:12 +0100

    fixed a typo

--- Toolchain-cross-mingw-linux.cmake
+++ Toolchain-cross-mingw-linux.cmake
@@ -14,7 +14,7 @@ set(CMAKE_CXX_COMPILER ${COMPILER_PREFIX}-g++-posix)
 # the target environment is located here
 set(CMAKE_FIND_ROOT_PATH  /usr/${COMPILER_PREFIX} ) #${USER_ROOT_PATH})
 
-# adjust the default behaviour of the FIND_XXX() commands:
+# adjust the default behavior of the FIND_XXX() commands:
 # search headers and libraries in the target environment, search
 # programs in the host environment
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
